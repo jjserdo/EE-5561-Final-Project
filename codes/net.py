d8a88515a4fefd6ee8b36ff7cdf3b389e6f77ce5
jjserdo <justineserdoncillo@gmail.com>
2023-12-15 12:59:14 -0600

    jj temp out

--- codes/net.py
+++ codes/net.py
@@ -41,7 +41,7 @@ class deepResUnet(nn.Module):
         self.decoding2 = res_block(256, 128, [1,1])
         self.decoding3 = res_block(128, 64, [1,1])
         self.upsamp = nn.Upsample(scale_factor=2, mode='bilinear')                  #I PUT SCALE_FACTOR=2 BECAUSE IT WAS LIKE THIS IN THE TUTORIAL, NOT SURE IF SHOOULD BE THAT HERE.
-        self.convlast = nn.Conv2d(64,num_classes, kernel_size=1)
+        self.convlast = nn.Conv2d(64, num_classes, kernel_size=1)
         self.sigmoid = nn.Sigmoid()
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=1)
@@ -78,9 +78,9 @@ class deepResUnet(nn.Module):
         Y1 = self.decoding1(Y1) + nn.Conv2d(512,256, kernel_size=1)(Y1)
         print(self.upsamp(Y1).shape)
         print(y2.shape)
-        Y2 = torch.cat(self.upsamp(Y1), nn.Conv2d(256,128, kernel_size=1)(y2))
+        Y2 = torch.cat(self.upsamp(Y1), nn.Conv2d(128,256, kernel_size=1)(y2))
         Y2 = self.decoding2(Y2) + nn.Conv2d(256,128, kernel_size=1)(Y2)
-        Y3 = torch.cat(self.upsamp(Y2), nn.Conv2d(128,64, kernel_size=1)(y1))
+        Y3 = torch.cat(self.upsamp(Y2), nn.Conv2d(64,128, kernel_size=1)(y1))
         Y3 = self.decoding3(Y3) + nn.Conv2d(128,64, kernel_size=1)(Y3)
         y = self.convlast(Y3)
         y = self.sigmoid(y)
